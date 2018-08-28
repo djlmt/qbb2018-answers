bfde994a6c7f4c2d8386f0ce0d62603d48bbfb61
Haolong Zhu <haolong_zhu@jhu.edu>
2018-08-28 14:16:34 -0400

    Day-2-Lunch-Exercise-5

---
+++ day2-lunch/day2-exercise-5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+s = 0
+t = 0
+a = 0
+#Input#
+if len(sys.argv) > 1:
+    #open from file#
+    f = open( sys.argv[1] )
+else:
+    #read from the stain by using <#
+    f = sys.stdin
+   
+for count, line in enumerate( f ):
+    #Alignments#
+    if line.startswith("SRR072893"):
+        fields = line.rstrip("\r\n").split("\t")
+        s = s+float(fields[4])
+        t = t+1
+             
+a = s/t
+
+print("The average MAPQ score is " + str(a) + ""."")
