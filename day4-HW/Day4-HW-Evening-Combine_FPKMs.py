d6e16e8efbea9843b334148c4d112799cdc658d4
Haolong Zhu <haolong_zhu@jhu.edu>
2018-08-30 23:48:42 -0400

    Day4-HW-Evening-Combine_FPKMs

--- day4-HW/Day4-HW-Evening-Combine_FPKMs.py
+++ day4-HW/Day4-HW-Evening-Combine_FPKMs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./Day4-HW-Evening-Exercise-1.py <samples.csv> (e.g. ./Day4-HW-Evening-Exercise-1.py ~/qbb2018/samples.csv)
+Usage: ./Day4-HW-Evening-Combine_FPKMs.py <samples.csv> (e.g. ./Day4-HW-Evening-Combine_FPKMs.py ~/qbb2018/samples.csv)
 In this case, ctab files are stored in "~/data/results/stringtie/", sample_name, "t_data.ctab".
 Create a single file all.csv that contains the FPKMs from all 16 samples in samples.csv.
 """
-
 
 import sys
 import os
 import pandas as pd
 
+#def#
 fpkms = {}
 
 #Sample input#
@@ -22,7 +22,7 @@ for index, sample_name, sex, stage in df.itertuples():
     fpkm = pd.read_csv(path, sep="\t", index_col="t_name").loc[:,"FPKM"]
     fpkms[sample_info] = fpkm
 
-#Output DataFrame#       
+#Output as DataFrame#       
 FPKM = pd.DataFrame(fpkms)
 #print(FPKM)
 FPKM.to_csv(sys.stdout, sep = ",")
