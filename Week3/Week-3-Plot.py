245add07d7ba58859ce76ca2f8b7456a1866eef0
Haolong Zhu <haolong_zhu@jhu.edu>
2018-09-28 01:05:05 -0400

    Week3-HW

--- Week3/Week-3-Plot.py
+++ Week3/Week-3-Plot.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./Week-2-Dotplot.py <Lastz_sorted file> <file name> 
-e.g. ./Week-2-Dotplot.py Lastz_sort short_reads_low
+Usage: ./Week-3-Plot.py calls.vcf
 
-Use lastz to align the contigs to the reference and produce a dotplot.
-The data need to be sorted by the end position ahead. Use the following command line: sort -k 5 -n Lastz > Lastz_sort.
+Input the data from the vcf file to plot the read depth distribution across each called variant
 """
 
 #Import#
@@ -14,37 +12,98 @@ import matplotlib.pyplot as plt
 
 #Def#
 DP_X_Value = []
-DP_Y_Value = []
-x = 1
-#Input & data processing#
-Input = open(sys.argv[1])
-for read in Input:
-    if read.startswith("#"):
-        continue
-    fields = read.rstrip("\r\n").split("\t")
-    DP_List = fields[7].split(";")
-    #print(DP)
-    DP = DP_List[7]
-    #print(DP)
-    DP_Value = DP.split("=")
-    #print(DP_Value[1])
-    DP_Y_Value.append(DP_Value[1])
-    DP_X_Value.append(x)
-    x = x+1
-#print(len(DP_X_Value), len(DP_Y_Value))
-#print(DP_X_Value, DP_Y_Value)
+DPV = []
+QRV = []
+AFV = []
+Tick = []
+header_count = 0
+absoult_count = 0
 
-#Plot
+#Input & data processing#
+for count, line in enumerate(open(sys.argv[1])):
+    "Skip the header"
+    if line.startswith("#"):
+        header_count = header_count +1
+        continue
+    "Grab the colum that contains the DP infromation"
+    field = line.rstrip("\r\n").split("\t")
+    "Grab the DP value"
+    Values = field[7].split(";")
+    #print(Values)
+    #print(Values[27])
+    DP_Value = Values[7].split("=")
+    #print(DP_Value[0], DP_Value[1])
+    DPV.append(DP_Value[1])
+    QR_Value = Values[27].split("=")
+    #print(QR_Value)
+    QRV.append(QR_Value[1])
+    AF_Value = Values[3].split("=")
+    AFV.append(AF_Value[1])
+    #print(AF_Value)
+    absoult_count = count - header_count + 1
+    DP_X_Value.append(absoult_count)
+    #if absoult_count%100 == 0:
+        #Tick.append(absoult_count)
+#print(len(DP_X_Value), len(DP_Value))
+#print(DP_X_Value)
+#print(DPV)
+#print(QRV)
+#print(AFV)
+
+#Plot#
 fig, ax = plt.subplots() 
 fig.set_size_inches(20, 12)
-ax.scatter(DP_X_Value, DP_Y_Value, alpha=0.18, s=1.68, color="blue")
+#plt.hist(DPV, bins=1600, color="purple")
+#plt.bar(DP_X_Value, DPV, align='center')
+ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
+#ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
+#ax.scatter(DP_X_Value, AFV, alpha=0.16, s=2.68, color="purple")
+#plt.legend(["Read depth", "Genotype quality", "Allele frequency"], loc = "center left", bbox_to_anchor = (1,0.5))
 #ax.set_yscale('log')
 #x.set_xlim(0,60000)
 #ax.set_ylim(0, 100000)
-ax.set_xlabel("Assembled contigs")
-ax.set_ylabel("Positions in reference")
-fig.suptitle(" alignments")
-fig.savefig("_dotplot.png")
+#plt.xticks(Tick)
+ax.set_xlabel("Positions")
+ax.set_ylabel("Read depth")
+fig.suptitle("Read depth distribution across each called variant")
+fig.savefig("Read_depth.png")
 plt.tight_layout()
 plt.close(fig)
 
+fig, ax = plt.subplots() 
+fig.set_size_inches(20, 12)
+#plt.hist(DPV, bins=1600, color="purple")
+#plt.bar(DP_X_Value, DPV, align='center')
+#ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
+ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
+#ax.scatter(DP_X_Value, AFV, alpha=0.16, s=2.68, color="purple")
+#plt.legend(["Read depth", "Genotype quality", "Allele frequency"], loc = "center left", bbox_to_anchor = (1,0.5))
+#ax.set_yscale('log')
+#x.set_xlim(0,60000)
+#ax.set_ylim(0, 100000)
+#plt.xticks(Tick)
+ax.set_xlabel("Positions")
+ax.set_ylabel("Genotype quality")
+fig.suptitle("Genotype quality distribution")
+fig.savefig("Genotype_quality.png")
+plt.tight_layout()
+plt.close(fig)
+
+fig, ax = plt.subplots() 
+fig.set_size_inches(20, 12)
+#plt.hist(DPV, bins=1600, color="purple")
+#plt.bar(DP_X_Value, DPV, align='center')
+#ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
+#ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
+ax.scatter(DP_X_Value, AFV, alpha=0.16, s=2.68, color="purple")
+#plt.legend(["Read depth", "Genotype quality", "Allele frequency"], loc = "center left", bbox_to_anchor = (1,0.5))
+#ax.set_yscale('log')
+#x.set_xlim(0,60000)
+#ax.set_ylim(0, 100000)
+#plt.xticks(Tick)
+ax.set_xlabel("Positions")
+ax.set_ylabel("Allele frequency")
+fig.suptitle("Allele frequency spectrum")
+fig.savefig("Allele_frequency_spectrum.png")
+plt.tight_layout()
+plt.close(fig)
