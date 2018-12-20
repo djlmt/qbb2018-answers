f25516427109e00a45c08b43f97688ef71df5b91
Haolong Zhu <haolong_zhu@jhu.edu>
2018-12-19 20:08:20 -0500

    Week3

--- Week3/Week-3-Plot.py
+++ Week3/Week-3-Plot.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./Week-3-Plot.py calls.vcf
+Usage: ./Week-3-Plot.py snpEff_calls.vcf
 
 Input the data from the vcf file to plot the read depth distribution across each called variant
 """
 
 #Import#
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 #Def#
@@ -50,12 +51,17 @@ for count, line in enumerate(open(sys.argv[1])):
 #print(QRV)
 #print(AFV)
 
+snpEff = [42.162, 10.092, 4.368, 0.093, 0.0, 0.001, 0.011, 43.272]
+Tags = ["Downstream", "Exon", "Intergenic", "Intron", "SS_Acceptor", "SS_Donor", "Splice Site", "Upstream"]
+
 #Plot#
-fig, ax = plt.subplots() 
-fig.set_size_inches(20, 12)
-#plt.hist(DPV, bins=1600, color="purple")
-#plt.bar(DP_X_Value, DPV, align='center')
-ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
+fig, axes = plt.subplots(nrows=2,ncols=2)
+axes = axes.flatten()
+fig.set_size_inches(20, 16)
+
+axes[0].set_title("Read depth distribution across each called variant")
+axes[0].hist(DPV, bins=96, color="purple")
+#ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
 #ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
 #ax.scatter(DP_X_Value, AFV, alpha=0.16, s=2.68, color="purple")
 #plt.legend(["Read depth", "Genotype quality", "Allele frequency"], loc = "center left", bbox_to_anchor = (1,0.5))
@@ -63,47 +69,46 @@ ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
 #x.set_xlim(0,60000)
 #ax.set_ylim(0, 100000)
 #plt.xticks(Tick)
-ax.set_xlabel("Positions")
-ax.set_ylabel("Read depth")
-fig.suptitle("Read depth distribution across each called variant")
-fig.savefig("Read_depth.png")
-plt.tight_layout()
-plt.close(fig)
+axes[0].set_yscale("log")
+axes[0].set_xlabel("Variants")
+axes[0].set_ylabel("Read depth_log")
 
-fig, ax = plt.subplots() 
-fig.set_size_inches(20, 12)
-#plt.hist(DPV, bins=1600, color="purple")
+axes[1].set_title("Genotype quality distribution")
+axes[1].hist(QRV, bins=156, color="blue")
 #plt.bar(DP_X_Value, DPV, align='center')
 #ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
-ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
+#ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
 #ax.scatter(DP_X_Value, AFV, alpha=0.16, s=2.68, color="purple")
 #plt.legend(["Read depth", "Genotype quality", "Allele frequency"], loc = "center left", bbox_to_anchor = (1,0.5))
 #ax.set_yscale('log')
 #x.set_xlim(0,60000)
 #ax.set_ylim(0, 100000)
 #plt.xticks(Tick)
-ax.set_xlabel("Positions")
-ax.set_ylabel("Genotype quality")
-fig.suptitle("Genotype quality distribution")
-fig.savefig("Genotype_quality.png")
-plt.tight_layout()
-plt.close(fig)
+#axes[1].set_yscale("log")
+axes[1].set_xlabel("Variants")
+axes[1].set_ylabel("Gene quality")
 
-fig, ax = plt.subplots() 
-fig.set_size_inches(20, 12)
+
+axes[2].set_title("Allele frequency spectrum")
+axes[2].hist(AFV, bins=86, color="red")
 #plt.hist(DPV, bins=1600, color="purple")
 #plt.bar(DP_X_Value, DPV, align='center')
 #ax.scatter(DP_X_Value, DPV, alpha=0.16, s=2.68, color="blue")
 #ax.scatter(DP_X_Value, QRV, alpha=0.16, s=2.68, color="red")
-ax.scatter(DP_X_Value, AFV, alpha=0.16, s=2.68, color="purple")
 #plt.legend(["Read depth", "Genotype quality", "Allele frequency"], loc = "center left", bbox_to_anchor = (1,0.5))
 #ax.set_yscale('log')
 #x.set_xlim(0,60000)
 #ax.set_ylim(0, 100000)
 #plt.xticks(Tick)
-ax.set_xlabel("Positions")
-ax.set_ylabel("Allele frequency")
-fig.suptitle("Allele frequency spectrum")
-fig.savefig("Allele_frequency_spectrum.png")
+axes[2].set_xlabel("Variants")
+axes[2].set_ylabel("Allele frequency")
+
+axes[3].set_title("Predicted effects of each variant")
+axes[3].bar(Tags, snpEff, color="pink")
+axes[3].set_xticklabels(Tags, rotation=45)
+axes[3].set_xlabel("Variant type")
+axes[3].set_ylabel("Percentage")
+
 plt.tight_layout()
+fig.savefig("Multi-panel-plot_Week-3_HW.png")
 plt.close(fig)
